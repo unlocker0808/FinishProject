58c84b7964e63be2091f3415acff3f251614d783
ncp204 <giodongnamayv3@gmail.com>
2021-05-30 22:10:33 +0700

    chỉnh lại smooth cho Scroll to top

--- public/assets/js/grayscale.js
+++ public/assets/js/grayscale.js
@@ -1,6 +1,9 @@
-(function ($) {
-  "use strict"; // Start of use strict
+jQuery(document).ready(function ($) {
+    scroll_top_duration = 700,
+    //grab the "back to top" link
+    $back_to_top = $('.back-to-top');
 
+  //hide or show the "back to top" link
   $(window).scroll(function () {
     if ($(this).scrollTop() > 200) {
       $('.back-to-top').fadeIn('slow');
@@ -8,47 +11,14 @@
       $('.back-to-top').fadeOut('slow');
     }
   });
-  $('.back-to-top').click(function () {
-    $('html, body').animate({ scrollTop: 0 }, 1500);
-    
+
+  //smooth scroll to top
+  $back_to_top.on('click', function (event) {
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0,
+    }, scroll_top_duration
+    );
   });
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 48)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function () {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
-  });
-
-  // Collapse Navbar
-  var navbarCollapse = function () {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
-
-})(jQuery); // End of use strict
+});
